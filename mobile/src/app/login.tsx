a0a3f8ac4b85db8a770e654a32aef8a4d5637f58
MuriloRibeiro01 <murilo.ribeiro2709@gmail.com>
2026-07-07 17:25:26 -0300

    feat: Criação da tela de login.

---
+++ mobile/src/app/login.tsx
@@ -0,0 +1,17 @@
+import { ScrollView, Text, StyleSheet } from "react-native";
+import { ThemedView } from "@/components/themed-view";
+
+export default function LoginScreen() {
+    return (
+        <ScrollView>
+            <ThemedView >
+
+            </ThemedView>
+        </ScrollView>
+        
+    );
+}
+
+const styles = StyleSheet.create({
+    
+})
